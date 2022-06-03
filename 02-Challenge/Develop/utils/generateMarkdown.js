eed54714bfebff5cd0d8cb62fbac2ad6f82b7023
Chandler Davis <echandlerdavis@gmail.com>
2022-06-03 15:25:18 -0700

    initial commit, notes on how to accomplish task at hand

---
+++ 02-Challenge/Develop/utils/generateMarkdown.js
@@ -0,0 +1,26 @@
+// TODO: Create a function that returns a license badge based on which license is passed in
+// If there is no license, return an empty string
+function renderLicenseBadge(license) {
+  if(license){
+//if there are lots of different kinds of licenses consider using a switch/case that was pretty easy
+  } else {
+    return ("");
+  }
+}
+
+// TODO: Create a function that returns the license link
+// If there is no license, return an empty string
+function renderLicenseLink(license) {}
+
+// TODO: Create a function that returns the license section of README
+// If there is no license, return an empty string
+function renderLicenseSection(license) {}
+
+// TODO: Create a function to generate markdown for README
+function generateMarkdown(data) {
+  return `# ${data.title}
+
+`;
+}
+
+module.exports = generateMarkdown;
